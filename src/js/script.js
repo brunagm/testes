cb8fa12fc2235a4d3a9ecd8c11e12c83c2ab063b
Bruna Grassi <54769698+brunagm@users.noreply.github.com>
2019-09-10 13:13:54 -0300

    Update script.js

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 numero = 0;
 function Diminuir() {
-  numero--;
+  if (numero > 0)
+  numero --;
   setValue(numero);
 }
 function Aumentar() {
@@ -11,5 +12,17 @@ function Aumentar() {
 function setValue(value) {
   document.getElementById('quantidade').value = value;
 }
-
 setValue(numero);
+
+function enviar(){    
+  var chkreact = document.getElementById("checkbox-1");
+  var chkvue = document.getElementById("checkbox-2");
+  var chkangular = document.getElementById("checkbox-3");
+  
+  if ((chkreact.checked || chkvue.checked || chkangular.checked) && (numero >0)) {
+      alert("Formulario enviado!")
+      }
+    else{
+      alert("Preencha todos os campos!");
+    }
+  } 
